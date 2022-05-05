07890afeaa37f1b11fb5f2b71b81391e48fe9728
omerokumus <omerokumus3@gmail.com>
2022-05-05 16:06:29 +0300

    deneme

--- src/QuickPuzzle.java
+++ src/QuickPuzzle.java
@@ -58,6 +58,7 @@ public class QuickPuzzle extends Application {
 
     private void openınfotext() {
         Stage stage = new Stage();
+        System.out.println(Math.pow(2, 3));
     }
 
     private void exitgame(Stage primaryStage) {
