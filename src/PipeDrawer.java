2c9b9614c4c27d03505fa2c1cdb1c41f24f7d622
tolga0001 <tolga_2001.fb@hotmail.com>
2022-05-08 13:38:24 +0300

    SENDİNG

---
+++ src/PipeDrawer.java
@@ -0,0 +1,40 @@
+import javafx.animation.PathTransition;
+import javafx.animation.Timeline;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Path;
+import javafx.util.Duration;
+
+public class PipeDrawer {
+    Path path;
+    GridPane pane;
+    Scene scene;
+
+    public PipeDrawer(Path path) {
+        this.path = path;
+    }
+
+    public void startAnimation() {
+        Circle circle = new Circle();
+        circle.setRadius(10);
+        circle.setFill(Color.RED);
+        PathTransition pt = new PathTransition();
+        pt.setPath(path);
+        pt.setNode(circle);
+        pt.setDuration(Duration.millis(4000));
+        pt.setAutoReverse(false);
+        pt.play();
+        Group group = new Group(circle);
+        scene = new Scene(group);
+
+    }
+
+    public Scene getScene() {
+        return scene;
+    }
+
+
+}
